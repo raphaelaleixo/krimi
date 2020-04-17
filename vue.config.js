d42c481ace58767c2c0bf463dd2f27b56387b873
Raphael Aleixo <raphael.aleixo@gmail.com>
2020-04-17 09:49:23 -0400

    Trying to make the pwa to work

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,13 @@ module.exports = {
     msTileColor: "#FFFFFE",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "white",
+    iconsPath: {
+      favicon32: "/favicon-32x32.png",
+      favicon16: "/favicon-16x16.png",
+      appleTouchIcon: "/apple-touch-icon.png",
+      maskIcon: "/safari-pinned-tab.svg",
+      msTileImage: "/mstile-144x144.png",
+    },
     workboxOptions: {
       skipWaiting: true,
       navigateFallback: "/index.html",
