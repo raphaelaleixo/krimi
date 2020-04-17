76785944f944f360763eba7f3baf864ab9905db0
Raphael Aleixo <raphael.aleixo@gmail.com>
2020-04-17 09:22:24 -0400

    Adjusting vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Krimi",
+    themeColor: "#FFFFFE",
+    msTileColor: "#FFFFFE",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "white",
+    workboxOptions: {
+      skipWaiting: true,
+      navigateFallback: "/index.html",
+    },
+  },
 };
