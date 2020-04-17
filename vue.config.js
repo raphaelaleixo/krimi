8e8eb3b8b0c82b6426023fd92b032e66f5e1eb78
Raphael Aleixo <raphael.aleixo@gmail.com>
2020-04-17 10:17:09 -0400

    Adjusting icons

--- vue.config.js
+++ vue.config.js
@@ -7,15 +7,16 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "white",
     iconsPath: {
-      favicon32: "/favicon-32x32.png",
-      favicon16: "/favicon-16x16.png",
-      appleTouchIcon: "/apple-touch-icon.png",
-      maskIcon: "/safari-pinned-tab.svg",
-      msTileImage: "/mstile-144x144.png",
+      favicon32: "./favicon-32x32.png",
+      favicon16: "./favicon-16x16.png",
+      appleTouchIcon: "./apple-touch-icon.png",
+      maskIcon: "./safari-pinned-tab.svg",
+      msTileImage: "./mstile-144x144.png",
     },
     workboxOptions: {
       skipWaiting: true,
       navigateFallback: "/index.html",
+      exclude: ".htaccess",
     },
   },
 };
